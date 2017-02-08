988eed3b6feb3cb02d94341f245c0efb00a2e9e1
Li <li3zhang@gmail.com>
2017-02-08 11:49:29 -0500

    df

--- txt-process.R
+++ txt-process.R
@@ -1,1 +1,16 @@
+cat("\014")
 setwd("~/Desktop/gilmore-charts")
+file <- file('raw/s01e01.txt', open='r')
+
+res <- readLines(file);
+res <- res[res != ""]
+speaker <- gsub( ":.+", "", res)
+words <- gsub(".+:", "", res)
+parsed <- data.frame(speaker, words, res)
+
+
+# colnames(parsed) <- c('original')
+
+
+close(file)
+
